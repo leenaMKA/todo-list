51db2cc34c31ff24c3acd37bf3ff68ced742a21b
leenaMKA <leena.mkarem@gmail.com>
2019-05-27 14:32:52 +0200

    Updates

---
+++ precache-manifest.1802e2ac672431e353f916483bb893fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32e2a12f38517c74d107",
+    "url": "/todo-list/static/css/main.1704cbd5.chunk.css"
+  },
+  {
+    "revision": "32e2a12f38517c74d107",
+    "url": "/todo-list/static/js/main.346013a0.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  },
+  {
+    "revision": "94c3165186f215582bb2",
+    "url": "/todo-list/static/js/2.c37d2ede.chunk.js"
+  },
+  {
+    "revision": "3cdca0e099678dcedd75528c4375b040",
+    "url": "/todo-list/index.html"
+  }
+];
